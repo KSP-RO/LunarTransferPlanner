2478116081e790894f9a45d349512fb61c9ae141
Test-Account-KSP <you@example.com>
2023-01-01 15:31:33 +0300

    Add estimated dV and time in parking orbit

--- LunarTransferPlanner.cs
+++ LunarTransferPlanner.cs
@@ -63,6 +63,20 @@ namespace LunarTransferPlanner
             }
             return (float)((int)(x * lol)) / lol;
         }
+        
+        // Math.Acosh does not seem to work in .NET 4
+        public static double Acosh(double x)
+        {
+            if (x >= 1) 
+            {
+                return Math.Log(x + Math.Sqrt(x*x - 1));
+            }
+            else
+            {
+                return Double.NaN;
+            }
+            
+        }
     }
 
     [KSPAddon(KSPAddon.Startup.FlightAndKSC, false)]
@@ -72,8 +86,12 @@ namespace LunarTransferPlanner
         Rect        windowRect = new Rect(100,100,-1,-1);
         bool        isWindowOpen = true;
         // gui stuff
+        const float tliAltitudeKM = 200f;       // Parking orbit altitude (circular orbit assumed). Not so important to be in the GUI.
         float       flightTime = 4f;
         float       nextTickFT = 0f;
+        bool        showParking0 = false;       // Expand/collapse time in parking orbit for launch now
+        bool        showParking1 = false;       // Expand/collapse time in parking orbit for first window
+        bool        showParking2 = false;       // Expand/collapse time in parking orbit for second window
         float       warpMargin = 60f;
         float       nextTickWM = 0f;
         string      windowTitle = "";
@@ -383,6 +401,201 @@ namespace LunarTransferPlanner
             return t;
         }
 
+        private double EstimateFlightTimeAfterTLI(CelestialBody target, double dV, bool movingTarget = true)
+        {
+            CelestialBody mainBody = target.referenceBody;
+            double gravParameter = mainBody.gravParameter;
+
+            // The formulas are from http://www.braeunig.us/space/orbmech.htm
+
+            // Assuming that the TLI is performed from a circular orbit with altitude = tliAltitudeKM
+            // Radius of the orbit, including the radius of the Earth
+            double r0 = mainBody.Radius + tliAltitudeKM * 1000;
+
+            // Orbital velocity after TLI
+            double v0 = Math.Sqrt(gravParameter / r0) + dV;
+
+            // Eccentricity after TLI (not the full formula, this is correct only at the periapsis)
+            double e = r0 * v0*v0 / gravParameter - 1;
+
+            // e == 1 would mean that the orbit is parabolic. No idea which formulas are applicable in this case.
+            // But it's so unlikely that I will just cheat and make such orbits slightly hyperbolic.
+            if (e == 1) 
+            {
+                // Increase velocity after TLI  by 0.1 m/s
+                v0 = v0 + 0.1;          
+                // Recalculate eccentricity
+                e = r0 * v0*v0 / gravParameter - 1;     
+            }
+
+            // Semi-major axis after TLI
+            double a = 1 / (2/r0 - v0*v0 / gravParameter);
+                        
+            // Altitude of the Moon at the time of the TLI 
+            double r1 = 0;
+
+            // The Moon is moving, so we need to know its altitude when the probe arrives
+            // For that we need to know the flight time, which is being calculated here in the first place
+            // It can be done in two steps: 
+            // 1) make a recursive call of EstimateFlightTimeAfterTLI to find out the approximate flight time,
+            //    based on the Moon's current altitude
+            // 2) use the Moon's altitude at this approximate time for further calculations
+            if (movingTarget)
+            {
+                // This is the "normal" call of EstimateFlightTimeAfterTLI from outside
+
+                // Making the recursive call of EstimateFlightTimeAfterTLI with movingTarget = false
+                double approxFlightTime = EstimateFlightTimeAfterTLI(target, dV, false);
+
+                // Altitude of the Moon at the approximate time of the TLI 
+                r1 = target.orbit.GetRadiusAtUT(Planetarium.GetUniversalTime() + approxFlightTime);
+            }
+            else
+            {
+                // This is the recursive call of EstimateFlightTimeAfterTLI
+
+                // Altitude of the Moon now
+                r1 = target.orbit.GetRadiusAtUT(Planetarium.GetUniversalTime());
+            }
+
+            // True anomaly when the vessel reaches the altitude of the Moon (r1)
+            double trueAnomaly1 = Math.Acos( (a * (1 - e*e) - r1) / (e * r1) );
+
+            // Time until the vessel reaches the altitude of the Moon (r1)
+            double t1 = 0;
+
+            // Elliptic orbit after TLI
+            if (e < 1) 
+            {
+                // Eccentric Anomaly when the vessel reaches the altitude of the Moon
+                double eccAnomaly1 = Math.Acos( (e + Math.Cos(trueAnomaly1)) / (1 + e * Math.Cos(trueAnomaly1)) );
+                double meanAnomaly1 = eccAnomaly1 - e * Math.Sin(eccAnomaly1);
+                t1 = meanAnomaly1 / Math.Sqrt( gravParameter / (a*a*a) );
+            }
+
+            // Parabolic orbit (e == 1) has been prevented earlier
+
+            // Hyperbolic orbit
+            if (e > 1) 
+            {
+                // Hyperbolic Eccentric Anomaly when the vessel reaches the altitude of the Moon
+                // Can't use Math.Acosh, it does not seem to work in .NET 4
+                double hEccAnomaly1 = Util.Acosh( (e + Math.Cos(trueAnomaly1)) / (1 + e * Math.Cos(trueAnomaly1)) );
+
+                t1 = Math.Sqrt( ((-a)*(-a)*(-a)) / gravParameter ) * (e * Math.Sinh(hEccAnomaly1) - hEccAnomaly1);
+            }
+
+            return t1;
+        }
+
+        private double EstimateFlightTimeBeforeTLI(CelestialBody target, Vector3d launchPos, double delayTime)
+        {
+            CelestialBody mainBody = target.referenceBody;
+            double gravParameter = mainBody.gravParameter;
+
+            // This code is copied from CalcOrbitForTime
+            Vector3d EarthPos = mainBody.position;
+            Vector3d EarthAxis = mainBody.angularVelocity.normalized;
+
+            double targetTime = Planetarium.GetUniversalTime() + flightTime * 24d * 3600d + delayTime;
+            Vector3d targetPos = target.getPositionAtUT(targetTime);
+
+            Vector3d upVector = Quaternion.AngleAxis((float)(delayTime * 360d / mainBody.rotationPeriod), EarthAxis) * (launchPos - EarthPos).normalized;
+            // end of copied code
+
+            // TLI takes place at the point of the orbit that is opposite to the future position of the Moon
+            Vector3d tliUpVector = (EarthPos - targetPos).normalized;
+
+            // Need to calculate 0-360 degree angle in prograde direction between upVector at launch and at the time of the TLI.
+            float rotationAngle;
+            Vector3 rotationAxis;
+            Quaternion rotationFromLaunchToTLI = new Quaternion();
+            rotationFromLaunchToTLI.SetFromToRotation(upVector, tliUpVector);
+            rotationFromLaunchToTLI.ToAngleAxis(out rotationAngle, out rotationAxis);
+            // Sometimes this rotation is prograde, sometimes retrograde. We need only prograde rotation.
+            if (Vector3d.Dot(rotationAxis.normalized, EarthAxis) > 0)
+            {
+                // rotationAxis is pointing roughly in the direction of Earth rotation axis => rotation is prograde => do nothing
+            }
+            else
+            {
+                // rotationAxis is pointing roughly opposite to Earth rotation axis => rotation is retrograde => rotate the other way
+                rotationAngle = (-rotationAngle) % 360;
+            }
+            
+            // The angle is converted to time in orbit
+            double orbitRadius = mainBody.Radius + tliAltitudeKM * 1000;
+            double orbitPeriod = 2 * Math.PI * Math.Sqrt( (orbitRadius*orbitRadius*orbitRadius) / gravParameter );
+            double flightTimeBeforeTLI = rotationAngle / 360 * orbitPeriod;
+
+            // Launch and maneuver planning take some time, say 15 minutes. If time to TLI is less than that, add an orbit.
+            if (flightTimeBeforeTLI < 15*60)
+            {
+                flightTimeBeforeTLI += orbitPeriod;
+            }
+
+            return flightTimeBeforeTLI;
+        }
+
+        private double EstimateDV(CelestialBody target, Vector3d launchPos)
+        {
+            float dV = float.NaN;
+
+            // Search in this range
+            const float minPossibleDV = 3000;
+            const float maxPossibleDV = 4200;
+            
+            // Current search range, will be gradually narrowed
+            float lowerBound = minPossibleDV;
+            float upperBound = maxPossibleDV;
+
+            // Max. 16 attempts, then return whatever value was found
+            for (int i = 0; i < 16; i++) 
+            {
+                // guess dV
+                dV = (lowerBound + upperBound) / 2;
+
+                // calculate flight time for this dV
+                double flightTimeAfterTLI = EstimateFlightTimeAfterTLI(target, dV);
+                double flightTimeBeforeTLI = EstimateFlightTimeBeforeTLI(target, launchPos, 0d);
+                double estimatedFlightTime = flightTimeBeforeTLI + flightTimeAfterTLI;
+                
+                // Debug.Log(i + " " + dV + " " + flightTime + " " + flightTimeBeforeTLI + " " + flightTimeAfterTLI + " " + estimatedFlightTime);
+                
+                if (Double.IsNaN(flightTimeAfterTLI))
+                {
+                    // dV is so low that target is unreachable, set lower bound to current guess and try again
+                    lowerBound = dV;
+                    continue;
+                }
+                else if (estimatedFlightTime > (flightTime * (24*60*60) + 60))
+                {
+                    // dV is too low, set lower bound to current guess and try again
+                    lowerBound = dV;
+                    continue;
+                }
+                else if (estimatedFlightTime < (flightTime * (24*60*60) - 60))
+                {
+                    // dV is too high, set upper bound to current guess and try again
+                    upperBound = dV;
+                    continue;
+                }
+                else 
+                {
+                    // correct flight time with this dV
+                    break;
+                }
+            }
+
+            if (dV == minPossibleDV || dV == maxPossibleDV)
+            {
+                // dV is incorrect, the correct value is outside the initial search range
+                dV = float.NaN;
+            }
+
+            return dV;
+        }
+
         private string FormatTime(double t)
         {
             t = Math.Round(t);
@@ -422,22 +635,91 @@ namespace LunarTransferPlanner
                     double firstLaunchETA = EstimateLaunchTime(target, launchPos, latitude, 0d);
                     double secondLaunchETA = EstimateLaunchTime(target, launchPos, latitude, firstLaunchETA + 3600d);
 
+                    double dV = EstimateDV(target, launchPos);
+
                     GUILayout.Space(4);
+                    GUILayout.Label("Required dV", GUILayout.ExpandWidth(true));
+                    GUILayout.Box(new GUIContent(String.Format("{0:0 m/s}", dV), "Required dV for the selected flight time"), GUILayout.MinWidth(100));
+
+                    GUILayout.Space(4);
+                    GUILayout.BeginHorizontal();
                     GUILayout.Label("Launch Now Incl", GUILayout.ExpandWidth(true));
-                    GUILayout.Box(new GUIContent($"{(launchOrbit.azimuth > 90d ? -launchOrbit.inclination : launchOrbit.inclination):F2}°",
+                    bool showParking0_pressed = GUILayout.Button("...", GUILayout.MinWidth(20));
+                    GUILayout.EndHorizontal();
+
+                    GUILayout.Space(4);
+                    GUILayout.Box(new GUIContent($"{(launchOrbit.azimuth > 90d ? -launchOrbit.inclination : launchOrbit.inclination):F2}\u00B0",
                         "Launch to this inclination now to reach a Lunar parking orbit"), GUILayout.MinWidth(100));
 
                     string tooltip = latitude >= target.orbit.inclination ?
                         "Launch at this time for an Easterly launch to Lunar parking orbit" :
                         "Launch at this time for a low inclination launch to Lunar parking orbit";
 
-                    GUILayout.Space(4);
-                    GUILayout.Label("First Window", GUILayout.ExpandWidth(true));
-                    GUILayout.Box(new GUIContent(FormatTime(firstLaunchETA), tooltip), GUILayout.MinWidth(100));
+                    if (showParking0_pressed)
+                    {
+                        showParking0 = !showParking0;
+                        // Doing this forces the window to be resized
+                        // Without it, the window will become bigger when controls expand, but never become smaller again
+                        windowRect = new Rect(windowRect.xMin, windowRect.yMin, -1, -1);
+                    }
+
+                    if (showParking0) 
+                    {
+                        double timeInOrbit0 = EstimateFlightTimeBeforeTLI(target, launchPos, 0d);
+                        GUILayout.Space(4);
+                        GUILayout.Label("Time in parking orbit", GUILayout.ExpandWidth(true));
+                        GUILayout.Box(new GUIContent(FormatTime(timeInOrbit0), ""), GUILayout.MinWidth(100));
+                    }
 
                     GUILayout.Space(4);
+                    GUILayout.BeginHorizontal();
+                    GUILayout.Label("First Window     ", GUILayout.ExpandWidth(true));
+                    bool showParking1_pressed = GUILayout.Button("...", GUILayout.MinWidth(20));
+                    GUILayout.EndHorizontal();
+
+                    GUILayout.Space(4);
+                    GUILayout.Box(new GUIContent(FormatTime(firstLaunchETA), tooltip), GUILayout.MinWidth(100));
+
+                    if (showParking1_pressed)
+                    {
+                        showParking1 = !showParking1;
+                        // Doing this forces the window to be resized
+                        // Without it, the window will become bigger when controls expand, but never become smaller again
+                        windowRect = new Rect(windowRect.xMin, windowRect.yMin, -1, -1);
+                    }
+
+                    if (showParking1) 
+                    {
+                        double timeInOrbit1 = EstimateFlightTimeBeforeTLI(target, launchPos, firstLaunchETA);
+                        GUILayout.Space(4);
+                        GUILayout.Label("Time in parking orbit", GUILayout.ExpandWidth(true));
+                        GUILayout.Box(new GUIContent(FormatTime(timeInOrbit1), ""), GUILayout.MinWidth(100));
+                    }
+
+                    GUILayout.Space(4);
+                    GUILayout.BeginHorizontal();
                     GUILayout.Label("Second Window", GUILayout.ExpandWidth(true));
+                    bool showParking2_pressed = GUILayout.Button("...", GUILayout.MinWidth(20));
+                    GUILayout.EndHorizontal();
+
+                    GUILayout.Space(4);
                     GUILayout.Box(new GUIContent(FormatTime(secondLaunchETA), tooltip), GUILayout.MinWidth(100));
+
+                    if (showParking2_pressed)
+                    {
+                        showParking2 = !showParking2;
+                        // Doing this forces the window to be resized
+                        // Without it, the window will become bigger when controls expand, but never become smaller again
+                        windowRect = new Rect(windowRect.xMin, windowRect.yMin, -1, -1);
+                    }
+
+                    if (showParking2) 
+                    {
+                        double timeInOrbit2 = EstimateFlightTimeBeforeTLI(target, launchPos, secondLaunchETA);
+                        GUILayout.Space(4);
+                        GUILayout.Label("Time in parking orbit", GUILayout.ExpandWidth(true));
+                        GUILayout.Box(new GUIContent(FormatTime(timeInOrbit2), ""));
+                    }
 
                     GUILayout.Label("Warp Margin (sec)", GUILayout.ExpandWidth(true));
                     MakeNumberEditField(ref warpMargin, ref nextTickWM, 0, 5f, 0f);
@@ -477,7 +759,7 @@ namespace LunarTransferPlanner
                         {
                             TimeWarp.fetch.WarpTo(Planetarium.GetUniversalTime() + firstLaunchETA - warpMargin);
                         }
-                }
+                    }
                 }
 
             GUILayout.EndVertical();
